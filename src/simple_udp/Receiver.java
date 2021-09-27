c84b5e3a855672b19913914a1ec9cb812b27b6e6
Gordon Agola <okothgordon10@gmail.com>
2021-09-27 13:25:48 +0300

    Initial commit

---
+++ src/simple_udp/Receiver.java
@@ -0,0 +1,43 @@
+package simple_udp;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class Receiver {
+	
+	
+	public Receiver() throws Exception {
+		DatagramSocket socket = new DatagramSocket(2020);
+		System.out.println("Receiver is running");
+		Scanner key = new Scanner(System.in);
+		
+		byte[] buffer = new byte[1500];//Maximum transmission Unit
+		DatagramPacket packet = new DatagramPacket(buffer,buffer.length);
+		
+		socket.receive(packet); //Retrieving sender's message
+		String message = new String(buffer).trim();
+		System.out.println("Received: "+message);
+		
+		InetAddress senders_address = packet.getAddress();
+		int senders_port = packet.getPort();
+		
+		System.out.println("Enter your message: ");
+		message = key.nextLine();
+		buffer = message.getBytes();
+		packet = new DatagramPacket(buffer,buffer.length,senders_address,senders_port);
+		socket.send(packet);
+		
+		System.out.println("Sent: "+message);
+	}
+	
+public static void main(String[] args) {
+	try {
+		new Receiver();
+	} catch (Exception e) {
+		// TODO: handle exception
+		e.printStackTrace();
+	}
+}
+}
